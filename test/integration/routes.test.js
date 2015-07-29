1efd90e24feb6dbcd1eeb9533b5888d49873387b
calebgregory <calebgregory@gmail.com>
2015-07-29 13:15:19 -0500

    Adding integration tests for routes

--- test/integration/routes.test.js
+++ test/integration/routes.test.js
@@ -4,7 +4,10 @@ var path = require('path');
 
 describe('routes', function() {
 
+  this.timeout(30000);
+
   var port = Math.floor(Math.random() * 30000 + 10000);
+  var url = 'http://localhost:'+ port;
 
   before(function() {
 
@@ -12,12 +15,49 @@ describe('routes', function() {
 
   });
 
+  it('should not respond to the /nonexistent route', function(done){
+
+    http.get(url+'/nonexistent', function(res) {
+      expect(res.statusCode).to.equal(403);
+      done();
+    });
+
+  });
+
   it('should respond to the /weather route', function(done){
 
-    http.get('http://localhost:'+port+'/weather', function(res) {
+    http.get(url+'/weather', function(res) {
+
+      var body = '';
+
       expect(res.statusCode).to.equal(200);
-      expect(res.body).to.contain('°');
-      done();
+
+      res.on('data',function(chunk) {
+        body+=chunk;
+      }).on('end',function() {
+        expect(body).to.contain('temperature');
+        done();
+      });
+
+    });
+
+  });
+
+  it('should respond to the /starwars route', function(done){
+
+    http.get(url+'/starwars', function(res) {
+
+      var body = '';
+
+      expect(res.statusCode).to.equal(200);
+
+      res.on('data',function(chunk) {
+        body+=chunk;
+      }).on('end',function() {
+        expect(body).to.contain('Hope');
+        done();
+      });
+
     });
 
   });
