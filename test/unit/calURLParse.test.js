a980a6cbaba3e6ad01c53a3d9b99361c88149b11
calebgregory <calebgregory@gmail.com>
2015-07-29 13:30:41 -0500

    Adding corrected test and command maker to the parser

--- test/unit/calURLParse.test.js
+++ test/unit/calURLParse.test.js
@@ -14,7 +14,7 @@ describe('calURLParse', function() {
     expect(parse('/cal/1/2015')).to.equal('./cal 1 2015');
   });
   it('should handle a month-last /cal/2015/1 route', function() {
-    expect(parse('/cal/2015/1')).to.equal('./cal/2015/1');
+    expect(parse('/cal/2015/1')).to.equal('./cal 1 2015');
   });
   it('should handle invalid routes', function() {
     expect(parse('/cal/foo')).to.equal('./cal foo');
